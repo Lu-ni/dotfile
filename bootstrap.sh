c05603af247fd1e193ae08763da28e33e2966126
Lucas Nicollier <lucas.nicollier@gmail.com>
2024-04-15 19:32:05 +0200

    fixy

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-git clone --recursive git://github.com/antoinealb/dotvim.git ~/.vim
+git clone --recursive https://github.com/Lu-ni/dotvim.git ~/.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/.vim/vimrc ~/.vimrc
 vim -c ":PluginInstall" -c ":qa"
